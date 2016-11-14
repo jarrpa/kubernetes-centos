8391a0d7e024dfdf0c8cb0daaf415641ceb46585
Jose A. Rivera <jarrpa@redhat.com>
2016-11-14 09:52:43 -0600

    Missing script exports and removal of reference to outdated script.

--- deploy/02-vars.sh
+++ deploy/02-vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export KUBECONFIG="/etc/kubernetes/admin.conf"
-heketi_secret=$(kubectl get sa heketi-service-account -o="go-template" --template="{{(index .secrets 0).name}}")
+export heketi_secret=$(kubectl get sa heketi-service-account -o="go-template" --template="{{(index .secrets 0).name}}")
 context=$(kubectl config current-context)
 cluster=$(kubectl config view -o jsonpath="{.contexts[?(@.name==\"$context\")].context.cluster}")
-kubeapi=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"$cluster\")].cluster.server}")
+export kubeapi=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"$cluster\")].cluster.server}")
